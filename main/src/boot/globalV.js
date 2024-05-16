8704f5457eda6a3684733140096e8fbba0cdacd0
OriginalSnow <70016672+IM2eR0@users.noreply.github.com>
2024-05-16 14:50:10 +0800

    Update globalV.js

--- main/src/boot/globalV.js
+++ main/src/boot/globalV.js
@@ -4,7 +4,7 @@ export default boot(async ({app}) => {
   // 请修改此项为你的后端地址
   // 如果你使用前后一体，请修改此项为后端显示的地址或反代地址
   // const yggLink = "https://yggdrasil.nekogan.com"
-  const yggLink = "https://yggdrasil.nekogan.com"
+  const yggLink = "http://gmod.ltd:54030"
 
 
   app.config.globalProperties.$yggApi = yggLink
